fd19594566c640225f250bfecb08b56723bffb21
Joseph Junker <joseph.jnk@gmail.com>
2012-07-13 16:25:09 -0700

    User CRUD page now selects from a list of availible roles

--- client/js/routes/users.coffee
+++ client/js/routes/users.coffee
@@ -13,96 +13,103 @@ define ["app/server", "app/notify", "routes/sidebar", "templates/sidebar", "temp
           departments: $('#editUser .departments').val()
         }
 
-      getNewUser = ->
-        {
-          firstName: ""
-          lastName: ""
-          email: ""
-          role: "Operator"
-          websites: ""
-          departments: ""
-        }
+
 
       server.ready ->
-        server.findUser {}, (err, users) ->
-          console.log "err retrieving users: #{err}" if err
-          sidebar {}, sbTemp
 
-          getUserById = (id) ->
-            for user in users
-              return user if user.id is id
+        server.getRoles (err, allowedRoles) ->
 
-          wireUpRow = (id) =>
-            editUserClicked = (evt) ->
+          getNewUser = ->
+            {
+              firstName: ""
+              lastName: ""
+              email: ""
+              role: "Operator"
+              websites: ""
+              departments: ""
+              allowedRoles: allowedRoles
+            }
+
+          server.findUser {}, (err, users) ->
+            console.log "err retrieving users: #{err}" if err
+            sidebar {}, sbTemp
+
+            getUserById = (id) ->
+              for user in users
+                return user if user.id is id
+
+            wireUpRow = (id) =>
+              editUserClicked = (evt) ->
+                evt.preventDefault()
+                currentUser = getUserById $(this).attr 'userId'
+                currentUser.allowedRoles = allowedRoles
+
+                $("#modalBox").html editUser user: currentUser
+                $('#editUser').modal()
+
+                $('#editUser .saveButton').click (evt) ->
+                  evt.preventDefault()
+
+                  fields = getFormFields()
+                  fields.id = currentUser.id
+
+                  server.saveUser fields, (err, savedUser) ->
+                    return notify.error "Error saving user: #{err}" if err?
+
+                    $("#userTableBody .userRow[userId=#{currentUser.id}]").replaceWith userRow user: savedUser
+                    wireUpRow(currentUser.id)
+                    $('#editUser').modal 'hide'
+
+                $('#editUser .cancelButton').click (evt) ->
+                  evt.preventDefault()
+                  $('#editUser').modal 'hide'
+
+              deleteUserClicked = (evt) ->
+                evt.preventDefault()
+                currentUser = getUserById $(this).attr 'userId'
+
+                $("#modalBox").html deleteUser user: currentUser
+                $('#deleteUser').modal()
+
+                $('#deleteUser .deleteButton').click (evt) ->
+                  evt.preventDefault()
+
+                  server.deleteUser currentUser.id, (err) ->
+                    return notify.error "Error deleting user: #{err}" if err?
+                    $("#userTableBody .userRow[userId=#{currentUser.id}]").remove()
+                    $('#deleteUser').modal 'hide'
+
+                $('#deleteUser .cancelButton').click ->
+                  $('#deleteUser').modal 'hide'
+
+              $("#userTableBody .userRow[userId=#{id}] .editUser").click editUserClicked
+              $("#userTableBody .userRow[userId=#{id}] .deleteUser").click deleteUserClicked
+
+            #Done with edit/delete handlers, now render page
+            $('#content').html templ users: users
+
+            #Wire up addUser
+            $('#addUser').click (evt) ->
               evt.preventDefault()
-              currentUser = getUserById $(this).attr 'userId'
 
-              $("#modalBox").html editUser user: currentUser
+              $("#modalBox").html editUser user: getNewUser()
               $('#editUser').modal()
 
               $('#editUser .saveButton').click (evt) ->
                 evt.preventDefault()
 
                 fields = getFormFields()
-                fields.id = currentUser.id
 
                 server.saveUser fields, (err, savedUser) ->
                   return notify.error "Error saving user: #{err}" if err?
-
-                  $("#userTableBody .userRow[userId=#{currentUser.id}]").replaceWith userRow user: savedUser
-                  wireUpRow(currentUser.id)
+                  users.push savedUser
+                  $("#userTableBody").append userRow user: savedUser
+                  wireUpRow(savedUser.id)
                   $('#editUser').modal 'hide'
 
               $('#editUser .cancelButton').click (evt) ->
                 evt.preventDefault()
                 $('#editUser').modal 'hide'
 
-            deleteUserClicked = (evt) ->
-              evt.preventDefault()
-              currentUser = getUserById $(this).attr 'userId'
-
-              $("#modalBox").html deleteUser user: currentUser
-              $('#deleteUser').modal()
-
-              $('#deleteUser .deleteButton').click (evt) ->
-                evt.preventDefault()
-
-                server.deleteUser currentUser.id, (err) ->
-                  return notify.error "Error deleting user: #{err}" if err?
-                  $("#userTableBody .userRow[userId=#{currentUser.id}]").remove()
-                  $('#deleteUser').modal 'hide'
-
-              $('#deleteUser .cancelButton').click ->
-                $('#deleteUser').modal 'hide'
-
-            $("#userTableBody .userRow[userId=#{id}] .editUser").click editUserClicked
-            $("#userTableBody .userRow[userId=#{id}] .deleteUser").click deleteUserClicked
-
-          #Done with edit/delete handlers, now render page
-          $('#content').html templ users: users
-
-          #Wire up addUser
-          $('#addUser').click (evt) ->
-            evt.preventDefault()
-
-            $("#modalBox").html editUser user: getNewUser()
-            $('#editUser').modal()
-
-            $('#editUser .saveButton').click (evt) ->
-              evt.preventDefault()
-
-              fields = getFormFields()
-
-              server.saveUser fields, (err, savedUser) ->
-                return notify.error "Error saving user: #{err}" if err?
-                users.push savedUser
-                $("#userTableBody").append userRow user: savedUser
-                wireUpRow(savedUser.id)
-                $('#editUser').modal 'hide'
-
-            $('#editUser .cancelButton').click (evt) ->
-              evt.preventDefault()
-              $('#editUser').modal 'hide'
-
-          #Attach handlers to all rows  
-          wireUpRow user.id for user in users
+            #Attach handlers to all rows  
+            wireUpRow user.id for user in users
--- public/js/routes/users.js
+++ public/js/routes/users.js
@@ -3,7 +3,7 @@
 
   define(["app/server", "app/notify", "routes/sidebar", "templates/sidebar", "templates/editUser", "templates/deleteUser", "templates/userRow"], function(server, notify, sidebar, sbTemp, editUser, deleteUser, userRow) {
     return function(args, templ) {
-      var getFormFields, getNewUser;
+      var getFormFields;
       if (!server.cookie('session')) {
         return window.location.hash = '/';
       }
@@ -17,56 +17,115 @@
           departments: $('#editUser .departments').val()
         };
       };
-      getNewUser = function() {
-        return {
-          firstName: "",
-          lastName: "",
-          email: "",
-          role: "Operator",
-          websites: "",
-          departments: ""
-        };
-      };
       return server.ready(function() {
-        return server.findUser({}, function(err, users) {
-          var getUserById, user, wireUpRow, _i, _len, _results,
-            _this = this;
-          if (err) {
-            console.log("err retrieving users: " + err);
-          }
-          sidebar({}, sbTemp);
-          getUserById = function(id) {
-            var user, _i, _len;
-            for (_i = 0, _len = users.length; _i < _len; _i++) {
-              user = users[_i];
-              if (user.id === id) {
-                return user;
-              }
-            }
+        return server.getRoles(function(err, allowedRoles) {
+          var getNewUser;
+          getNewUser = function() {
+            return {
+              firstName: "",
+              lastName: "",
+              email: "",
+              role: "Operator",
+              websites: "",
+              departments: "",
+              allowedRoles: allowedRoles
+            };
           };
-          wireUpRow = function(id) {
-            var deleteUserClicked, editUserClicked;
-            editUserClicked = function(evt) {
-              var currentUser;
+          return server.findUser({}, function(err, users) {
+            var getUserById, user, wireUpRow, _i, _len, _results,
+              _this = this;
+            if (err) {
+              console.log("err retrieving users: " + err);
+            }
+            sidebar({}, sbTemp);
+            getUserById = function(id) {
+              var user, _i, _len;
+              for (_i = 0, _len = users.length; _i < _len; _i++) {
+                user = users[_i];
+                if (user.id === id) {
+                  return user;
+                }
+              }
+            };
+            wireUpRow = function(id) {
+              var deleteUserClicked, editUserClicked;
+              editUserClicked = function(evt) {
+                var currentUser;
+                evt.preventDefault();
+                currentUser = getUserById($(this).attr('userId'));
+                currentUser.allowedRoles = allowedRoles;
+                $("#modalBox").html(editUser({
+                  user: currentUser
+                }));
+                $('#editUser').modal();
+                $('#editUser .saveButton').click(function(evt) {
+                  var fields;
+                  evt.preventDefault();
+                  fields = getFormFields();
+                  fields.id = currentUser.id;
+                  return server.saveUser(fields, function(err, savedUser) {
+                    if (err != null) {
+                      return notify.error("Error saving user: " + err);
+                    }
+                    $("#userTableBody .userRow[userId=" + currentUser.id + "]").replaceWith(userRow({
+                      user: savedUser
+                    }));
+                    wireUpRow(currentUser.id);
+                    return $('#editUser').modal('hide');
+                  });
+                });
+                return $('#editUser .cancelButton').click(function(evt) {
+                  evt.preventDefault();
+                  return $('#editUser').modal('hide');
+                });
+              };
+              deleteUserClicked = function(evt) {
+                var currentUser;
+                evt.preventDefault();
+                currentUser = getUserById($(this).attr('userId'));
+                $("#modalBox").html(deleteUser({
+                  user: currentUser
+                }));
+                $('#deleteUser').modal();
+                $('#deleteUser .deleteButton').click(function(evt) {
+                  evt.preventDefault();
+                  return server.deleteUser(currentUser.id, function(err) {
+                    if (err != null) {
+                      return notify.error("Error deleting user: " + err);
+                    }
+                    $("#userTableBody .userRow[userId=" + currentUser.id + "]").remove();
+                    return $('#deleteUser').modal('hide');
+                  });
+                });
+                return $('#deleteUser .cancelButton').click(function() {
+                  return $('#deleteUser').modal('hide');
+                });
+              };
+              $("#userTableBody .userRow[userId=" + id + "] .editUser").click(editUserClicked);
+              return $("#userTableBody .userRow[userId=" + id + "] .deleteUser").click(deleteUserClicked);
+            };
+            $('#content').html(templ({
+              users: users
+            }));
+            $('#addUser').click(function(evt) {
               evt.preventDefault();
-              currentUser = getUserById($(this).attr('userId'));
               $("#modalBox").html(editUser({
-                user: currentUser
+                user: getNewUser()
               }));
               $('#editUser').modal();
               $('#editUser .saveButton').click(function(evt) {
                 var fields;
                 evt.preventDefault();
                 fields = getFormFields();
-                fields.id = currentUser.id;
                 return server.saveUser(fields, function(err, savedUser) {
                   if (err != null) {
                     return notify.error("Error saving user: " + err);
                   }
-                  $("#userTableBody .userRow[userId=" + currentUser.id + "]").replaceWith(userRow({
+                  users.push(savedUser);
+                  $("#userTableBody").append(userRow({
                     user: savedUser
                   }));
-                  wireUpRow(currentUser.id);
+                  wireUpRow(savedUser.id);
                   return $('#editUser').modal('hide');
                 });
               });
@@ -74,68 +133,14 @@
                 evt.preventDefault();
                 return $('#editUser').modal('hide');
               });
-            };
-            deleteUserClicked = function(evt) {
-              var currentUser;
-              evt.preventDefault();
-              currentUser = getUserById($(this).attr('userId'));
-              $("#modalBox").html(deleteUser({
-                user: currentUser
-              }));
-              $('#deleteUser').modal();
-              $('#deleteUser .deleteButton').click(function(evt) {
-                evt.preventDefault();
-                return server.deleteUser(currentUser.id, function(err) {
-                  if (err != null) {
-                    return notify.error("Error deleting user: " + err);
-                  }
-                  $("#userTableBody .userRow[userId=" + currentUser.id + "]").remove();
-                  return $('#deleteUser').modal('hide');
-                });
-              });
-              return $('#deleteUser .cancelButton').click(function() {
-                return $('#deleteUser').modal('hide');
-              });
-            };
-            $("#userTableBody .userRow[userId=" + id + "] .editUser").click(editUserClicked);
-            return $("#userTableBody .userRow[userId=" + id + "] .deleteUser").click(deleteUserClicked);
-          };
-          $('#content').html(templ({
-            users: users
-          }));
-          $('#addUser').click(function(evt) {
-            evt.preventDefault();
-            $("#modalBox").html(editUser({
-              user: getNewUser()
-            }));
-            $('#editUser').modal();
-            $('#editUser .saveButton').click(function(evt) {
-              var fields;
-              evt.preventDefault();
-              fields = getFormFields();
-              return server.saveUser(fields, function(err, savedUser) {
-                if (err != null) {
-                  return notify.error("Error saving user: " + err);
-                }
-                users.push(savedUser);
-                $("#userTableBody").append(userRow({
-                  user: savedUser
-                }));
-                wireUpRow(savedUser.id);
-                return $('#editUser').modal('hide');
-              });
             });
-            return $('#editUser .cancelButton').click(function(evt) {
-              evt.preventDefault();
-              return $('#editUser').modal('hide');
-            });
+            _results = [];
+            for (_i = 0, _len = users.length; _i < _len; _i++) {
+              user = users[_i];
+              _results.push(wireUpRow(user.id));
+            }
+            return _results;
           });
-          _results = [];
-          for (_i = 0, _len = users.length; _i < _len; _i++) {
-            user = users[_i];
-            _results.push(wireUpRow(user.id));
-          }
-          return _results;
         });
       });
     };

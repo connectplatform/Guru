2d6f529211e8e33b20655a6a31df0ac44a8bf6c8
bitmage <brandonjmason@gmail.com>
2012-08-17 04:04:01 -0700

    merge

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,13 +1,7 @@
 (function() {
 
-<<<<<<< HEAD
   define(["dermis", "app/addMiddleware", "routes/sidebar", "templates/sidebar", 'app/onPageLoad'], function(dermis, addMiddleware, sidebar, sbTemp, onPageLoad) {
-    dermis.route('/newChat');
-=======
-  define(["dermis", "app/addMiddleware", "routes/sidebar", "templates/sidebar"], function(dermis, addMiddleware, sidebar, sbTemp) {
-    var operatorPages;
     dermis.route('/newChat?');
->>>>>>> ccff887221e30cb31240c31b9df48aaea42560cf
     dermis.route('/visitorChat/:id');
     dermis.route('/', 'routes/index');
     dermis.route('/login');

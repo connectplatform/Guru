15667122b40026508c11cc3934d45ab37f829d2b
Joseph Junker <joseph.jnk@gmail.com>
2012-08-28 11:10:07 -0700

    changed visitorChat to use setup/teardown

--- client/js/routes/visitorChat.coffee
+++ client/js/routes/visitorChat.coffee
@@ -1,21 +1,22 @@
 define ["app/server", "app/pulsar", "app/notify", "templates/newChat", "templates/chatMessage", "templates/serverMessage"],
   (server, pulsar, notify, newChat, chatMessage, serverMessage) ->
-    ({chatId}, templ) ->
-
+    channel: {}
+    setup: ({chatId}, templ) ->
+      self = this
       server.ready ->
         server.visitorCanAccessChannel chatId, (err, canAccess) ->
           return window.location.hash = '/newChat' unless canAccess
 
           $("#content").html templ()
           $("#message-form #message").focus()
-          channel = pulsar.channel chatId
+          self.channel = pulsar.channel chatId
 
           $(".message-form").submit (evt) ->
             evt.preventDefault()
             unless $(".message").val() is ""
               message = $(".message").val()
 
-              channel.emit 'clientMessage', {message: message, session: server.cookie 'session'}
+              self.channel.emit 'clientMessage', {message: message, session: server.cookie 'session'}
 
               $(".message").val("")
               $(".chat-display-box").scrollTop($(".chat-display-box").prop("scrollHeight"))
@@ -28,17 +29,15 @@ define ["app/server", "app/pulsar", "app/notify", "templates/newChat", "template
             notify.error "Error loading chat history: #{err}" if err
             appendChat msg for msg in history
 
-            channel.on 'serverMessage', appendChat
-            channel.on 'chatEnded', ->
-              #TODO need to make sure disconnection occurs server side as well
+            self.channel.on 'serverMessage', appendChat
+            self.channel.on 'chatEnded', ->
               $(".chat-display-box").append serverMessage message: "The operator has ended the chat"
-              channel.removeAllListeners 'serverMessage'
+              self.channel.removeAllListeners 'serverMessage'
               $(".message-form").hide()
 
-            # stop listening for pulsar events when we leave the page
-            ran = false
-            window.rooter.hash.listen (newHash) ->
-              unless ran
-                ran = true
-                channel.removeAllListeners 'serverMessage'
-                channel.removeAllListeners 'chatEnded'
+    teardown: (cb) ->
+      self = this
+      ran = true
+      self.channel.removeAllListeners 'serverMessage'
+      self.channel.removeAllListeners 'chatEnded'
+      cb()
--- public/js/routes/visitorChat.js
+++ public/js/routes/visitorChat.js
@@ -2,63 +2,67 @@
 (function() {
 
   define(["app/server", "app/pulsar", "app/notify", "templates/newChat", "templates/chatMessage", "templates/serverMessage"], function(server, pulsar, notify, newChat, chatMessage, serverMessage) {
-    return function(_arg, templ) {
-      var chatId;
-      chatId = _arg.chatId;
-      return server.ready(function() {
-        return server.visitorCanAccessChannel(chatId, function(err, canAccess) {
-          var appendChat, channel;
-          if (!canAccess) {
-            return window.location.hash = '/newChat';
-          }
-          $("#content").html(templ());
-          $("#message-form #message").focus();
-          channel = pulsar.channel(chatId);
-          $(".message-form").submit(function(evt) {
-            var message;
-            evt.preventDefault();
-            if ($(".message").val() !== "") {
-              message = $(".message").val();
-              channel.emit('clientMessage', {
-                message: message,
-                session: server.cookie('session')
-              });
-              $(".message").val("");
-              $(".chat-display-box").scrollTop($(".chat-display-box").prop("scrollHeight"));
+    return {
+      channel: {},
+      setup: function(_arg, templ) {
+        var chatId, self;
+        chatId = _arg.chatId;
+        self = this;
+        return server.ready(function() {
+          return server.visitorCanAccessChannel(chatId, function(err, canAccess) {
+            var appendChat;
+            if (!canAccess) {
+              return window.location.hash = '/newChat';
             }
-            return false;
-          });
-          appendChat = function(data) {
-            return $(".chat-display-box").append(chatMessage(data));
-          };
-          return server.getChatHistory(chatId, function(err, history) {
-            var msg, ran, _i, _len;
-            if (err) {
-              notify.error("Error loading chat history: " + err);
-            }
-            for (_i = 0, _len = history.length; _i < _len; _i++) {
-              msg = history[_i];
-              appendChat(msg);
-            }
-            channel.on('serverMessage', appendChat);
-            channel.on('chatEnded', function() {
-              $(".chat-display-box").append(serverMessage({
-                message: "The operator has ended the chat"
-              }));
-              channel.removeAllListeners('serverMessage');
-              return $(".message-form").hide();
-            });
-            ran = false;
-            return window.rooter.hash.listen(function(newHash) {
-              if (!ran) {
-                ran = true;
-                channel.removeAllListeners('serverMessage');
-                return channel.removeAllListeners('chatEnded');
+            $("#content").html(templ());
+            $("#message-form #message").focus();
+            self.channel = pulsar.channel(chatId);
+            $(".message-form").submit(function(evt) {
+              var message;
+              evt.preventDefault();
+              if ($(".message").val() !== "") {
+                message = $(".message").val();
+                self.channel.emit('clientMessage', {
+                  message: message,
+                  session: server.cookie('session')
+                });
+                $(".message").val("");
+                $(".chat-display-box").scrollTop($(".chat-display-box").prop("scrollHeight"));
               }
+              return false;
+            });
+            appendChat = function(data) {
+              return $(".chat-display-box").append(chatMessage(data));
+            };
+            return server.getChatHistory(chatId, function(err, history) {
+              var msg, _i, _len;
+              if (err) {
+                notify.error("Error loading chat history: " + err);
+              }
+              for (_i = 0, _len = history.length; _i < _len; _i++) {
+                msg = history[_i];
+                appendChat(msg);
+              }
+              self.channel.on('serverMessage', appendChat);
+              return self.channel.on('chatEnded', function() {
+                $(".chat-display-box").append(serverMessage({
+                  message: "The operator has ended the chat"
+                }));
+                self.channel.removeAllListeners('serverMessage');
+                return $(".message-form").hide();
+              });
             });
           });
         });
-      });
+      },
+      teardown: function(cb) {
+        var ran, self;
+        self = this;
+        ran = true;
+        self.channel.removeAllListeners('serverMessage');
+        self.channel.removeAllListeners('chatEnded');
+        return cb();
+      }
     };
   });
 
